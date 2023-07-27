e0a94a8f3308b86601586e527cbcbcb5fd196002
Lucas <lucas.82@hotmail.com.ar>
2023-07-27 11:43:53 -0300

    desafico completo

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const UlElement = document.querySelector(".lista");
+  const LiElements = UlElement.querySelectorAll("li");
+
+  for (let i = 0; i < LiElements.length; i++) {
+    UlElement.removeChild(LiElements[i]);
+  }
+
+  cosasQueAprendimos.forEach((e) => {
+    const newLi = document.createElement("li");
+    newLi.textContent = e.tema;
+    newLi.className = e.class;
+    UlElement.appendChild(newLi);
+  });
+}
 
 main();
